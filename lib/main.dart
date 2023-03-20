9a8718fe0236c3e88e57fded102c6e54662e257e
Anderson P Soares <ayo@outlook.com.br>
2023-03-20 18:13:15 -0300

    add style and theme

--- lib/main.dart
+++ lib/main.dart
@@ -14,9 +14,28 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'DeliMeals',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        // primarySwatch: Colors.pink,
+        canvasColor: const Color.fromRGBO(255, 254, 229, 1),
+        colorScheme: const ColorScheme.light().copyWith(
+          primary: Colors.pink,
+          secondary: Colors.amber,
+          error: Colors.red,
+        ),
+        fontFamily: 'Raleway',
+        textTheme: ThemeData.light().textTheme.copyWith(
+            bodyLarge: const TextStyle(
+              color: Color.fromRGBO(20, 51, 51, 1),
+            ),
+            bodyMedium: const TextStyle(
+              color: Color.fromRGBO(20, 51, 51, 1),
+            ),
+            titleLarge: const TextStyle(
+              fontSize: 20,
+              fontFamily: 'RobotoCondensed',
+              fontWeight: FontWeight.bold,
+            )),
       ),
-      home: CategoriesScreen(),
+      home: const CategoriesScreen(),
     );
   }
 }
